28db6ff22613b2b41ef68f7fae8bc8a82295307f
anothersnoopy <gk64610012@163.com>
2017-03-20 20:03:39 +0800

    Whole structure has been completed, v1.0 release!

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include<iostream>
+#include<fstream>
 #include"function.cpp"
 using namespace std;
 
 int main()
 {
 	Good good;
-	char finish;
-	cout<<"Have you finished learn words?"<<endl;
-	cout<<"(y/n)";
-	cin>>finish;
-	if(finish == 'y')
-	{
-		good.word();
-	}
+	Write write;
+	Data data;
+
 	
-	cout<<"total_word_score:"<<good.show_word<<endl;
+	data.read();//read yesterday's summary
+	write.date();//write today's date
+	write.table_head();
+	good.word();
+	good.outhome();
+	good.cn_paper();
+	good.en_paper();
+	write.summary();
+
+	
+	cout<<endl;
+	cout<<"ok,finished."<<endl;
 
 	return 0;
 }
